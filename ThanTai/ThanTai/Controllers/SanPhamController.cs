4f84019b9d209f7702ac19f09d144d704958273b
Cao Hao <you@example.com>
2025-03-29 12:39:38 +0700

    12:39 29/3/2025

--- ThanTai/ThanTai/Controllers/SanPhamController.cs
+++ ThanTai/ThanTai/Controllers/SanPhamController.cs
@@ -22,24 +22,74 @@ namespace ThanTai.Controllers
             return View(sanPhams);
         }
 
-        public IActionResult TuLanhView()
+        public async Task<IActionResult> TuLanhView(int? ThuongHieuID, int? SoCanhCua, string Gia, string CongNgheInverter)
         {
-            var tuLanh = _context.SanPham
-                .Include(sp => sp.HinhAnhSanPham) // Load hình ảnh sản phẩm
-                .Where(sp => sp.LoaiSanPhamID == 5)
-                .ToList();
+            decimal? GiaMin = null;
+            decimal? GiaMax = null;
 
-            return View(tuLanh);
+            if (!string.IsNullOrEmpty(Gia))
+            {
+                var parts = Gia.Split('-').Select(p => decimal.TryParse(p, out var val) ? val : (decimal?)null).ToArray();
+                GiaMin = parts.Length > 0 ? parts[0] : null;
+                GiaMax = parts.Length > 1 ? parts[1] : null;
+            }
+
+            var query = _context.SanPham
+                .Include(sp => sp.ThuongHieu)
+                .Include(sp => sp.GiaTriThuocTinh)
+                    .ThenInclude(gt => gt.ThuocTinh)
+                .Include(sp => sp.HinhAnhSanPham)
+                .Where(sp => sp.LoaiSanPhamID == 5) // Chỉ lấy sản phẩm loại tủ lạnh
+                .AsQueryable();
+
+            if (ThuongHieuID.HasValue)
+            {
+                query = query.Where(sp => sp.ThuongHieuID == ThuongHieuID.Value);
+            }
+
+            if (SoCanhCua.HasValue)
+            {
+                query = query.Where(sp => sp.GiaTriThuocTinh.Any(gt =>
+                    gt.ThuocTinh.TenThuocTinh == "Số cánh cửa" && gt.GiaTri == SoCanhCua.Value.ToString()
+                ));
+            }
+
+            if (GiaMin.HasValue)
+            {
+                query = query.Where(sp => sp.GiaSauKhiGiam >= GiaMin.Value);
+            }
+
+            if (GiaMax.HasValue)
+            {
+                query = query.Where(sp => sp.GiaSauKhiGiam <= GiaMax.Value);
+            }
+
+            if (!string.IsNullOrEmpty(CongNgheInverter))
+            {
+                query = query.Where(sp => sp.GiaTriThuocTinh.Any(gt =>
+                    gt.ThuocTinh.TenThuocTinh == "Công nghệ Inverter" && gt.GiaTri == CongNgheInverter
+                ));
+            }
+
+            var sanPhams = await query.ToListAsync();
+
+            ViewBag.ThuongHieus = await _context.ThuongHieu.ToListAsync();
+            ViewBag.SelectedThuongHieu = ThuongHieuID;
+            ViewBag.SelectedSoCanh = SoCanhCua;
+            ViewBag.SelectedCongNghe = CongNgheInverter;
+
+            return View(sanPhams);
         }
 
         public IActionResult MayLanhView()
         {
             var mayLanh = _context.SanPham
-                .Include(sp => sp.HinhAnhSanPham) // Load hình ảnh sản phẩm
+                .Include(sp => sp.HinhAnhSanPham)
                 .Where(sp => sp.LoaiSanPhamID == 9)
                 .ToList();
 
             return View(mayLanh);
         }
+       
     }
 }
--- ThanTai/ThanTai/obj/Debug/net8.0/ThanTai.AssemblyInfo.cs
+++ ThanTai/ThanTai/obj/Debug/net8.0/ThanTai.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ThanTai")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+702acf51376a5d92177198bfee5f0bcceaa970cf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01f95ff87224c4333ecdf41304cd68c5e0c83306")]
 [assembly: System.Reflection.AssemblyProductAttribute("ThanTai")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ThanTai")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
